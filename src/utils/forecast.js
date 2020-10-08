e05ec7f597850efec0c3d1c02e0fab42e7223bb9
Adarsh Srivastav <adarsh1999srivastav@gmail.com>
2020-10-09 00:20:57 +0530

    added min and max temp

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -5,12 +5,14 @@ const forecast = (lat, lan ,  callback ) => {
     const url = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lan}&exclude=alerts&appid=77267d8f8fba1649207ec9516016d293`
 
     request({url , json: true}, (error, {body}) => {
+        
         if (error) {
             callback('Unable to connect to weather services' , undefined)
         } else if (body.message) {
             callback('Unable to find location' , undefined)
         } else {
-            callback(undefined, `${body.daily[0].weather[0].description} It is currently ${body.current.temp} degrees out. There wind speed is ${body.current.wind_speed}.`)
+            
+            callback(undefined, `${body.daily[0].weather[0].description} It is currently ${body.current.temp} degrees out. There wind speed is ${body.current.wind_speed}. max. temp : ${body.daily[0].temp.max} , min. temp: ${body.daily[0].temp.min}`)
         }
     })
 }
